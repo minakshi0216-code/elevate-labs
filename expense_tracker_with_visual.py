00a6f5cad379cb59f88405e62940cb62a3cddac5
Minakshi <minakshi0216@gmail.com>
2025-12-23 16:55:39 +0530

    expense_tracker_with_visual.py

--- expense_tracker_with_visual.py
+++ expense_tracker_with_visual.py
@@ -16,8 +16,8 @@ def load_expenses():
     try:
         df = pd.read_csv(file_path)
         total = df["Amount"].sum()
-        total_label.config(text=f"Total Expense: ₹{total}")
-        messagebox.showinfo("Success", "Expense file loaded successfully!")
+        total_label.config(text=f"Total Expense: {total}")
+        messagebox.showinfo("Success", "Expense file loaded successfully")
 
     except Exception as e:
         messagebox.showerror("Error", str(e))
@@ -25,7 +25,7 @@ def load_expenses():
 
 def show_bar_chart():
     if df is None:
-        messagebox.showwarning("Warning", "Please load expense file first!")
+        messagebox.showwarning("Warning", "Please load expense file first")
         return
 
     category_sum = df.groupby("Category")["Amount"].sum()
@@ -39,7 +39,7 @@ def show_bar_chart():
 
 def show_pie_chart():
     if df is None:
-        messagebox.showwarning("Warning", "Please load expense file first!")
+        messagebox.showwarning("Warning", "Please load expense file first")
         return
 
     category_sum = df.groupby("Category")["Amount"].sum()
@@ -63,8 +63,9 @@ Button(root, text="Load Expense CSV", command=load_expenses).pack(pady=10)
 Button(root, text="Show Bar Chart", command=show_bar_chart).pack(pady=10)
 Button(root, text="Show Pie Chart", command=show_pie_chart).pack(pady=10)
 
-total_label = Label(root, text="Total Expense: ₹0", font=("Arial", 12))
+total_label = Label(root, text="Total Expense: 0", font=("Arial", 12))
 total_label.pack(pady=20)
 
 root.mainloop()
+
 
